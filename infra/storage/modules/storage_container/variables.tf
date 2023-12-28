bef15754891bc1e467e4ab5777ee35bb54012cfa
Ashrene Roy <ashreneroy@gmail.com>
2023-12-28 16:34:51 +0530

    feat(storage): Add tf module for storage container

---
+++ infra/storage/modules/storage_container/variables.tf
@@ -0,0 +1,10 @@
+variable "storage_account_name" {
+  type        = string
+  description = "The name of the storage account"
+}
+
+variable "storage_container_name" {
+  type        = string
+  description = "The name of the storage container"
+}
+
