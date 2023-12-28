94636181dea4c003062657ad67f5859f986247c8
Ashrene Roy <ashreneroy@gmail.com>
2023-12-28 17:43:48 +0530

    feat(inventory): Add tf script to set up Legacy Inventory API in APIM

---
+++ infra/apim/legacy-inventory-api/main.tf
@@ -0,0 +1,90 @@
+terraform {
+  required_version = ">=1.3"
+  backend "azurerm" {
+    resource_group_name  = "ecomcart"
+    storage_account_name = "ecomcartstoragedev"
+    container_name       = "terraform-backend"
+    key                  = "actions.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.2"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+module "api" {
+  source = "../modules/api"
+  api = {
+    name                = var.api_name
+    revision            = var.api_revision
+    display_name        = var.api_display_name
+    path                = var.api_path
+    content_format      = var.api_content_format
+    content_value       = var.api_content_value
+  }
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+}
+
+module "named_value_1" {
+  source = "../modules/named_value"
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+  named_value = {
+    name         = var.named_value_name_1
+    display_name = var.named_value_display_name_1
+    value        = var.named_value_value_1
+  }
+}
+
+module "named_value_2" {
+  source = "../modules/named_value"
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+  named_value = {
+    name         = var.named_value_name_2
+    display_name = var.named_value_display_name_2
+    value        = var.named_value_value_2
+  }
+}
+
+module "api_policy" {
+  source = "../modules/api_policy"
+  depends_on = [ module.api, module.named_value_1, module.named_value_2 ]
+  api = {
+    name                = var.api_name
+    policy_file_path    = var.api_policy_file_path
+    revision            = var.api_revision
+    display_name        = var.api_display_name
+    path                = var.api_path
+    content_format      = var.api_content_format
+    content_value       = var.api_content_value
+  }
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+}
+
+# module "operation_policy" {
+#   source = "../modules/operation_policy"
+#   depends_on = [ module.api, module.named_value_1, module.named_value_2 ]
+#   api = {
+#     name                = var.api_name
+#     policy_file_path    = var.api_policy_file_path
+#     revision            = var.api_revision
+#     display_name        = var.api_display_name
+#     path                = var.api_path
+#     content_format      = var.api_content_format
+#     content_value       = var.api_content_value
+#   }
+#   api_management_name = var.api_management_name
+#   resource_group_name = var.resource_group_name
+#   operation = {
+#     id                = var.operation_id
+#     policy_file_path  = var.operation_policy_file_path
+#   }
+# }
---
+++ infra/apim/legacy-inventory-api/variables.tf
@@ -0,0 +1,84 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group"
+}
+
+variable "api_name" {
+  type        = string
+  description = "The name of the API"
+}
+
+variable "api_policy_file_path" {
+  type        = string
+  description = "File path for the API policy"
+}
+
+variable "api_revision" {
+  type        = string
+  description = "Revision number of the API"
+}
+
+variable "api_display_name" {
+  type        = string
+  description = "Display name of the API"
+}
+
+variable "api_path" {
+  type        = string
+  description = "Path of the API"
+}
+
+variable "api_content_format" {
+  type        = string
+  description = "Content format of the API (e.g., swagger-json)"
+}
+
+variable "api_content_value" {
+  type        = string
+  description = "Content value or URL for the API definition"
+}
+
+variable "api_management_name" {
+  type        = string
+  description = "The name of the API Management service"
+}
+
+variable "named_value_name_1" {
+  type        = string
+  description = "Name of the named value"
+}
+
+variable "named_value_display_name_1" {
+  type        = string
+  description = "Display name of the named value"
+}
+
+variable "named_value_value_1" {
+  type        = string
+  description = "Value of the named value"
+}
+
+variable "named_value_name_2" {
+  type        = string
+  description = "Name of the named value"
+}
+
+variable "named_value_display_name_2" {
+  type        = string
+  description = "Display name of the named value"
+}
+
+variable "named_value_value_2" {
+  type        = string
+  description = "Value of the named value"
+}
+
+# variable "operation_id" {
+#   type        = string
+#   description = "ID of the operation"
+# }
+
+# variable "operation_policy_file_path" {
+#   type        = string
+#   description = "File path for the operation policy"
+# }
